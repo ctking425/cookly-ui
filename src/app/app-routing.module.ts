2707a63c257e485871b1ba953d000fb2e62efed1
Chris King <Chris King>
2017-03-25 22:30:05 -0400

    Fixed some issues, added a service.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { RecipeComponent } from './recipe/recipe.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/recipies',
+    redirectTo: '/recipes',
     pathMatch: 'full'
   },
   {
